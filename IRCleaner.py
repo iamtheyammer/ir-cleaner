3851c01b66b0e5d5dae6b1ccb8538ec16bf7fb8e
iamtheyammer <32084206+iamtheyammer@users.noreply.github.com>
2018-07-02 23:30:07 -0700

    Changed output formats + more
    - Added minimal input checking (if your last character is a comma, it tells you and removes it. if your first/last character isn't a number, it will throw a human-readable error).
    - Added LIRC support.
    - If a number is already a multiple of a hundred, it won't bother rounding it.
    - Added the output format in the welcome message.

--- IRCleaner.py
+++ IRCleaner.py
@@ -9,9 +9,18 @@
 
 import math
 
-arduino = True
-# if arduino is true, this will format it for use with an arduino, starting with unsigned int cleanedIR[] = { and ending with a closed curly brace and a semicolon
-# make sure that your True or False starts with a capital letter!
+## OUTPUT VARIABLE
+
+## 0) Unformatted list (array). Example: [34534700, 64600, 100, 400, 100, 0, 0, 547200]
+
+## 1) Arduino: this will format it for use with an arduino, starting with unsigned int cleanedIR[] = { and ending with a closed curly brace and a semicolon. Example: unsigned int cleanedIR[] = {100,5600,0,346500,0,0,100,700,563500,57500};
+
+## 2) LIRC (linux infrared remote control): For use with LIRC on any linux machine, like a raspberry pi. Assumes that a number like -1242 is a space and that a number like 1242 is a pulse. (if you don't know what that last sentence means, don't worry about it!)
+
+output = 2
+
+## Now, onto the code:
+# Begin definitions
 
 def roundUp(x):
   return int(math.ceil(x / 100.0)) * 100
@@ -20,8 +29,8 @@ def roundDown(x):
   return int(math.floor(x / 100.0)) * 100
 
 def round(x):
-  x = abs(x)
-  if int(str(x)[-2:]) < 50:
+  x = abs(x) # remove the negativeness, if it exists by using absolute value 
+  if int(str(x)[-2:]) < 50: # choose whether to round up or down based on whether the number is below 50
     return roundDown(x)
   else:
     return roundUp(x)
@@ -30,16 +39,54 @@ def listToString(list):
    string = ','.join(str(e) for e in list)
    return string
 
-toClean = input('What is the IR string to clean? Paste it in, comma seperated.\n')
-toClean = toClean.split(',')
-#print(toClean)
+# End definitons; begin main code block, output agnostic
+
+if output == 0: # display the output format when running
+  toClean = input('What is the IR string to clean? Paste it in, comma seperated. [Output format: Not formatted]\n')
+elif output == 1:
+  toClean = input('What is the IR string to clean? Paste it in, comma seperated. [Output format: Arduino]\n')
+elif output == 2:
+  toClean = input('What is the IR string to clean? Paste it in, comma seperated. [Output format: LIRC]\n')
+else:
+  raise Exception('[FATAL] Your output variable is set to an invalid value. Please set it to 0, 1 or 2.')
+
+
+if toClean[-1:] == ',': # check user unput
+  print('[WARN] Your input ends with a comma. Please avoid this in the future.')
+  toClean = toClean[:-1] # remove that ending comma
+elif toClean[-1:].isdigit() == False or toClean[:1].isdigit() == False:
+  raise Exception('[FATAL] Your string does not start/end with a number.')
+
+toClean = toClean.split(',') # turn the text input to an array (python says a dictionary or list)
 
 cleaned = []
-for i in range(0, len(toClean)):
+
+for i in range(0, len(toClean)): # this for loop does the work of taking the dirty data and cleaning it
+  if toClean[i][-2:] == '00': # if the number ends in 00 it's already a multiple of a hundred
+    continue
   cleaned.append(round(int(toClean[i])))
 
-if arduino == True:
+# End main code block; starting the customised output
+
+if output == 0: # basically just print the array
+  print('\nOutput (not formatted):\n\n' + str(cleaned) + '\n\n\n')
+elif output == 1: # print the array surrounded in static text, for arduino
   print('')
-  print('\n\n\nunsigned int cleanedIR[] = {' + listToString(cleaned) + '}; // cleaned ir signal by https://github.com/iamtheyammer/ir-cleaner\n\n\n')
-else:
-  print(cleaned)
+  print('\nOutput (formatted for Arduino):\n\nunsigned int cleanedIR[] = {' + listToString(cleaned) + '}; // cleaned ir signal by https://github.com/iamtheyammer/ir-cleaner\n\n\n')
+elif output == 2: # alternate between space and pulse
+  print('\nOutput (formatted for LIRC):\n\n')
+  final = '' # init the final string
+  if int(toClean[0]) < 0: # check if we should start with a space or pulse by seeing if the first number was negative
+    counter = 1
+  else:
+    counter = 0
+  for i in range(0, len(cleaned)): # for the length of the cleaned data, concatenate it
+    if counter == 0: 
+      final += 'space ' + str(cleaned[i]) + '\n'
+      counter += 1
+    elif counter == 1:
+      final += 'pulse ' + str(cleaned[i]) + '\n'
+      counter = 0
+  print(final)
+
+# End output
--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # ir-cleaner
-A simple python script to clean IR codes so that they work better. Basically, it just rounds your numbers.
+A simple python script to clean IR codes so that they work better. Basically, it just rounds your numbers and formats them.
 
-## Usage
+With some effort and help from @AnalysIR, I realised that the codes I was getting from my Arduino weren't working because they weren't cleaned. @AnalysIR cleaned them for me, and this script does that same cleaning process. (after the cleaning, my air conditioner turned on and off at the Uno's command!)
+
+Every time I plan on using raw IR codes, I will definitely clean them and I reccommend you do too.
+
+## Install/Usage
+0. Capture a raw IR code. For most devices, I'd try [this](https://github.com/z3t0/Arduino-IRremote/blob/master/examples/IRrecvDumpV2/IRrecvDumpV2.ino) script, from the [IRLib/Arduino-IRremote library](https://github.com/z3t0/Arduino-IRremote), and using the hex code (starts with `0x`) from it (effectively skipping the use of this tool). The hex codes don't always work, and if they don't I'd reccomend you try the following air conditioner instructions. If you're trying to control an air conditoner or something odd, use [this](https://www.analysir.com/blog/wp-content/uploads/2014/03/Arduino_Record_Long_AirConditioner_Infrared_Signals_10.txt) script on an arduino (seems to only work on an Uno), and that's where this tool really comes in handy. If you're using the script in the previous sentence, and you've got some data, let's clean it!
 1. Download the script. There's a little clone/download button on the homepage for this repository, so click that and download ZIP.
-2. Open the script and set the arduino variable to True/False (yes, the caps matters). If true, it will format the output as an unsigned int for arduino.
+2. Open the script in a text editor and set the output variable as follows:
+  - 0) Unformatted list (array). Example: [34534700, 64600, 100, 400, 100, 0, 0, 547200]
+  - 1) Arduino: this will format it for use with an arduino, starting with unsigned int cleanedIR[] = { and ending with a closed curly brace and a semicolon. Example: unsigned int cleanedIR[] = {100,5600,0,346500,0,0,100,700,563500,57500};
+  - 2) LIRC (linux infrared remote control): For use with LIRC on any linux machine, like a raspberry pi. Assumes that a number like -1242 is a space and that a number like 1242 is a pulse. (if you don't know what that last sentence means, don't worry about it!)
 3. Run the script, in pretty much any python compiler. (Open, then F5 in IDLE, or on a mac `python3 ./IRCleaner.py`)
 4. Enjoy your cleaned code!
 
+## Issues or questions?
+Open an issue on this repository, and maybe (just maybe!) tag @AnalysIR in said issue.
+
+## Protocol Support
+If you've got another protocol that you'd like custom output for, either code it in yourself and open a pull request or open an issue and maybe I'll add it. No guarantees on that though.
+
+
+## Credits
+
+Thank you to @AnalysIR for demonstrating the effect cleaning IR signals can do and their help solving [my issue](https://github.com/z3t0/Arduino-IRremote/issues/579) when trying to control my air conditioner.
+
+I wrote this script, though.
